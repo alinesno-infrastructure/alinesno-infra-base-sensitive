cbf34a8db3f81d8eec0785451c8b85ae1c2259ca
switchII <luoandon@gmail.com>
2023-10-03 11:15:56 +0800

    feat:集成敏感词界面

--- alinesno-infra-base-sensitive-admin/src/views/operation-workspace/countStatics.vue
+++ alinesno-infra-base-sensitive-admin/src/views/operation-workspace/countStatics.vue
@@ -2,7 +2,7 @@
     <div>
         <el-row class="acp-dashboard-panel" :gutter="20">
 
-            <el-col class="panel-col" :span="12">
+            <el-col class="panel-col" :span="19">
                 <div class="grid-content">
                     <div class="panel-header">
                         <div class="header-title"><i class="fa-solid fa-link"></i> 最新运行统计</div>
@@ -11,15 +11,7 @@
 
                         <div class="count-box">
                             <ul>
-                                <li class="count-box-item">
-                                    <div class="count-box-item-title">运行中</div>
-                                    <div class="count-box-item-number">280</div>
-                                </li>
-                                <li class="count-box-item">
-                                    <div class="count-box-item-title">运行中</div>
-                                    <div class="count-box-item-number">280</div>
-                                </li>
-                                <li class="count-box-item">
+                                <li class="count-box-item aip-item-count" style="width:calc(20% - 15px)" v-for="item in 5" :key="item">
                                     <div class="count-box-item-title">运行中</div>
                                     <div class="count-box-item-number">280</div>
                                 </li>
@@ -29,7 +21,7 @@
                     </div>
                 </div>
             </el-col>
-            <el-col class="panel-col" :span="12">
+            <el-col class="panel-col" :span="5">
                 <div class="grid-content">
                     <div class="panel-header">
                         <div class="header-title"><i class="fa-solid fa-link"></i> 最新运行统计</div>
@@ -38,15 +30,7 @@
 
                         <div class="count-box">
                             <ul>
-                                <li class="count-box-item">
-                                    <div class="count-box-item-title">运行中</div>
-                                    <div class="count-box-item-number">280</div>
-                                </li>
-                                <li class="count-box-item">
-                                    <div class="count-box-item-title">运行中</div>
-                                    <div class="count-box-item-number">280</div>
-                                </li>
-                                <li class="count-box-item">
+                                <li class="count-box-item aip-item-count" style="width:calc(50% - 15px)" v-for="item in 2" :key="item">
                                     <div class="count-box-item-title">运行中</div>
                                     <div class="count-box-item-number">280</div>
                                 </li>
