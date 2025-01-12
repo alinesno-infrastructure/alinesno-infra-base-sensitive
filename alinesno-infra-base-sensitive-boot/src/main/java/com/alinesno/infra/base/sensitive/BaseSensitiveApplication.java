acfaa4bf5a0b9b6240da2c50a75e91973935a492
张婷(Tina) <tinaAip@gmail.com>
2025-01-13 07:56:26 +0800

    refactor: 优化BaseSensitiveApplication类，移除不必要的注解和方法

--- alinesno-infra-base-sensitive-boot/src/main/java/com/alinesno/infra/base/sensitive/BaseSensitiveApplication.java
+++ alinesno-infra-base-sensitive-boot/src/main/java/com/alinesno/infra/base/sensitive/BaseSensitiveApplication.java
@@ -1,31 +1,20 @@
 package com.alinesno.infra.base.sensitive;
 
-import com.alinesno.infra.common.facade.enable.EnableActable;
-import com.alinesno.infra.common.web.adapter.sso.enable.EnableInfraSsoApi;
 import com.alinesno.infra.common.web.log.aspect.LogAspect;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 /**
  * 集成一个Java开发示例工具
- * @author LuoAnDong
- * @since 2023年8月3日 上午6:23:43
+ * @author luoxiaodong
+ * @version 1.0.0
  */
-@EnableActable
-@EnableInfraSsoApi
-@MapperScan("com.alinesno.infra.base.sensitive.mapper")
 @SpringBootApplication
 public class BaseSensitiveApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BaseSensitiveApplication.class, args);
-	}
-
-	@Bean
-	public LogAspect getLogAspect(){
-		return new LogAspect() ;
 	}
 
 }
