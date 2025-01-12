4cd2740bfbb76e0433cf04365676a20f3bd14f65
张婷(Tina) <tinaAip@gmail.com>
2025-01-13 07:56:26 +0800

    fix: 注释掉 SensitiveWordService 中的敏感词业务对象自动注入

--- alinesno-infra-base-sensitive-domain/src/main/java/com/alinesno/infra/base/sensitive/word/SensitiveWordService.java
+++ alinesno-infra-base-sensitive-domain/src/main/java/com/alinesno/infra/base/sensitive/word/SensitiveWordService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class SensitiveWordService {
 
-    @Autowired
+//    @Autowired
     private SensitiveWordBs sensitiveWordBs;
 
     /**
